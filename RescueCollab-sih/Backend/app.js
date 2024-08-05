d93e43641e447a2fae6af57d41625d21f83f23dc
Yakshine Anannya Gudapati <120457036+YakshineAnannyaGudapati@users.noreply.github.com>
2024-08-05 11:02:31 +0530

    Update app.js

--- RescueCollab-sih/Backend/app.js
+++ RescueCollab-sih/Backend/app.js
@@ -1,25 +1,23 @@
 import express from "express";
+import cors from "cors";
 import agencyRoutes from './routes/agencyRoutes.js';
 import alertRoutes from './routes/alertRoutes.js';
 import disasterRoutes from './routes/disasterRoutes.js';
 import resourceRoutes from './routes/resourceRoutes.js';
-import cors from "cors";
 
 const app = express();
 
 // Middlewares
 app.use(express.json());
-app.use(cors({
-  origin: 'https://rescueagencycollab.vercel.app',
-}));
+app.use(cors()); // Configure CORS as needed
 
 // Routes
-app.use('agency', agencyRoutes);
-app.use('alert', alertRoutes);
-app.use('disaster', disasterRoutes);
-app.use('resource', resourceRoutes);
+app.use('/agency', agencyRoutes);
+app.use('/alert', alertRoutes);
+app.use('/disaster', disasterRoutes);
+app.use('/resource', resourceRoutes);
 
-// Rest Api  
+// Rest API  
 app.get('/', (req, res) => {
   res.send("<h1>Welcome to Rescue Connect</h1>");
 });
